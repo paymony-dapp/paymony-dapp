89d67a6c55c83939356aa47db20843694db47f34
Emeka Allison <emekaallison4@gmail.com>
2022-06-28 02:11:51 +0100

    Feat: Subscription page

---
+++ pages/dashboard/subscription/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../../../components/Layout';
+
+const Subscription = () => {
+  return (
+    <Layout>
+      <div className='mx-auto max-w-screen-xl mt-8'>
+        <h1 className='text-zinc-100'>Hello Bomber</h1>
+      </div>
+    </Layout>
+  );
+};
+
+export default Subscription;
