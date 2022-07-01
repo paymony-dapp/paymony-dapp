d0ce0ebb0115a09b76a19e779ab645e3e0ab09d1
Emeka Allison <emekaallison4@gmail.com>
2022-07-01 03:43:27 +0100

    Feat: Subscribe page

---
+++ pages/subscribe/[id].tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IdPage = () => {
+  return (
+    <main className='min-h-screen w-screen bg-slate-900 text-zinc-100'>
+      IdPage
+    </main>
+  );
+};
+
+export default IdPage;
