8fbf0c444c83cff0f3840addd9aec1392db485b5
olatobra-felix <olatubora1996@gmail.com>
2022-07-03 22:30:31 +0100

    subscription feature added

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import Card from '../../components/dashboard/Card';
+import Table from '../../components/dashboard/Table';
 import Layout from '../../components/Layout';
 import { trpcClient } from '../../utils/trpcClient';
 
@@ -17,7 +18,7 @@ const Dashboard = () => {
 
   return (
     <Layout>
-      <h1 className='text-zinc-100 text-xl font-bold'>Dashboard</h1>
+      <h1 className='text-zinc-100 text-xl font-bold mb-6'>Dashboard</h1>
       <section className='grid sm:grid-col-2 md:grid-cols-4 lg:grid-col-4 gap-x-4'>
         <Card title='Active Subscriptions' price='20' />
         <Card title='Canceled Subscriptions' price='03' />
@@ -28,6 +29,9 @@ const Dashboard = () => {
           squircle={Squircle}
           wallet={wallet}
         />
+      </section>
+      <section className='my-6'>
+        <Table />
       </section>
     </Layout>
   );
