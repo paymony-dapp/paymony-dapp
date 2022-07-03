35cc891ab0862269e3feb51988a539afdc162562
Emeka Allison <emekaallison4@gmail.com>
2022-07-03 12:36:32 +0100

    Feat: Builders and test update

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,3 +1,4 @@
+import { generateAvatar } from '../utils/generateAvatar';
 import { generateString } from '../utils/generateString';
 
 test('Should return a random string with desired length', () => {
@@ -7,4 +8,10 @@ test('Should return a random string with desired length', () => {
 
 test('Generate function much have a default length of 5', () => {
   expect(generateString()).toHaveLength(5);
+});
+
+test('Generate image function should return a valid image url', () => {
+  const urlRegex =
+    /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
+  expect(generateAvatar()).toMatch(urlRegex);
 });
